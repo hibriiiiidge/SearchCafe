c1100b8bfbfa02bf758b507bb19a4e8255c9af94
Yasunori Takahashi <hibriiiiidge@gmail.com>
2017-05-02 20:53:27 +0900

    git test2

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,15 +1,15 @@
 class Shop < ActiveRecord::Base
-    #belongs_to :user
-    belongs_to :owner
-    has_many :likes, dependent: :destroy
-    has_many :shopimages, :inverse_of => :shop, dependent: :destroy
-    accepts_nested_attributes_for :shopimages, allow_destroy: true
-    has_many :menuimages, :inverse_of => :shop, dependent: :destroy
-    accepts_nested_attributes_for :menuimages, allow_destroy: true
-    has_many :messagerooms, dependent: :destroy
-    has_many :messages, dependent: :destroy
+  # belongs_to :user
+  belongs_to :owner
+  has_many :likes, dependent: :destroy
+  has_many :shopimages, inverse_of: :shop, dependent: :destroy
+  accepts_nested_attributes_for :shopimages, allow_destroy: true
+  has_many :menuimages, inverse_of: :shop, dependent: :destroy
+  accepts_nested_attributes_for :menuimages, allow_destroy: true
+  has_many :messagerooms, dependent: :destroy
+  has_many :messages, dependent: :destroy
 
-    mount_uploader :image1, ImageUploader
-    geocoded_by :address
-    after_validation :geocode
+  mount_uploader :image1, ImageUploader
+  geocoded_by :address
+  after_validation :geocode
 end
